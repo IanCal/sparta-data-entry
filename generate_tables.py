851311b4ae953992b11631d47ed5cb7d7262454a
Ian Calvert <ianjcalvert@gmail.com>
2017-02-06 23:48:13 +0000

    Script for creating tables for easy loading into matlab

---
+++ generate_tables.py
@@ -0,0 +1,38 @@
+import os
+import json
+from csv import DictWriter
+
+def get_donor(donor_id):
+	donor_filename = os.path.join('data', 'donors', donor_id, 'donor_data.json')
+	return json.load(open(donor_filename))
+
+def get_all_donors():
+	donor_folder = os.path.join('data','donors')
+	return os.listdir(donor_folder)
+
+file_descriptions = {
+	'volume_and_age.csv': {
+		'volume': ['initial_evaluation', 'volume'],
+		'ph': ['initial_evaluation', 'ph'],
+		'viscosity': ['initial_evaluation', 'viscosity'],
+		'age': ['questionnaire', 'age'],
+	},
+	'proton_scans_ph.csv': {
+		'end_ph': ['pellets', 'eighty_percent', 'proton', 'tend', 'ph'],
+		'ph': ['initial_evaluation', 'ph'],
+	},
+}
+
+for filename, description in file_descriptions.items():
+	with open(os.path.join('tables', filename), 'w') as f_out:
+		writer = DictWriter(f_out, sorted(description.keys()))
+		writer.writeheader()
+		for donor in get_all_donors():
+			donor_data = get_donor(donor)
+			required_data = {}
+			for header, key_list in description.items():
+				element = donor_data
+				for key in key_list:
+					element = element[key]
+				required_data[header] = element
+			writer.writerow(required_data)
